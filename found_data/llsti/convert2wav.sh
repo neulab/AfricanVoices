a472c79e7a5b55bc8d8803ddddb464b490f66ae5
Perez Ogayo <perezogayo@gmail.com>
2022-04-04 21:42:08 -0400

    Delete found_data directory

--- found_data/llsti/convert2wav.sh
+++
@@ -1,3 +0,0 @@
-cd lpc
-for i in *.res; do bname=`basename $i .res`; $ESTDIR/bin/ch_wave $i -otype riff -o $bname.rres; $FLITEDIR/testsuite/lpc_resynth -res $bname.rres $bname.lpc ../wav/$bname.wav; done
-cd ..
